68f70a7b84cd05bec840449be3c006cc5db55887
Naqvi <120029770+ihasnainaqvi@users.noreply.github.com>
2022-12-23 18:24:15 +0500

    heyyyyyyy

---
+++ web/script.js
@@ -0,0 +1,514 @@
+// let a=prompt("input number")
+
+// let b=prompt("input 2nd number")
+
+// let c=prompt("input 3rd number")
+
+// console.log(a+b+c)
+
+
+// let marks= prompt("enter your grade")
+
+// if (marks >=90){
+//     console.log("A1")
+// }
+// else if (marks >=80){
+//     console.log("A+")
+// }
+// else if (marks >=70){
+//     console.log("A")
+// }
+// else if (marks >=60){
+//     console.log("B")
+// }
+// else if (marks >=50){
+//     console.log("C")
+// }
+// else if (marks >=40){
+//     console.log("D")
+// }
+// else if (marks >=35){
+//     console.log("F")
+// }
+
+
+// let a = prompt("Do you have money");
+
+// let b = prompt("Do you veihcel");
+
+// if (a === "money"|| b ==="car"){
+//     console.log("Buy the fruits")
+// }else if (a === "money" || b === "bike"){
+//     console.log("Do not buy the fruits")
+// }
+// else if (a === "nomoney" || b=== "car"){
+//     console.log("It's too high don't even go to the market")
+// }
+// else{
+//     console.log("you have no money to buy fruits")
+// }
+
+
+// let percentage= prompt("Enter your marks");
+
+// if (percentage >=70 && percentage <=100){
+//     if (percentage >= 90){
+//         let result = "A+"
+//         document.write("Your grade is "+ result);
+//     }else if (percentage >= 80){
+//         result = "A1"
+//         document.write("Your Grade is "+ result)
+//     }else if(percentage >= 70){
+//          result = "A"
+//         document.write("Your Grade is "+ result)
+//     }
+
+// }else if(percentage >= 60 && percentage <= 70){
+//      result = "B"
+//     document.write("Your Grade is "+ result)
+// }else if (percentage >= 50 && percentage <= 60){
+//   result = "C"
+//     document.write("Your Grade is " + result)
+// }else if (percentage >=40 && percentage <=50){
+//     result = "D"
+//     document.write("Your Grade is " + result)
+// }
+// else if (percentage < 40 && percentage <=40){
+//      result = "F"
+//     document.write("Your grade is " + result)
+// }else {
+//     document.write("Kindly enter valid percentage")
+// }
+// switch (result) {
+//     case "A+":
+//     document.write("pass")
+//     break;
+
+//     case "A1":
+//     document.write("pass")
+//     break;
+
+//     case "A":
+//     document.write("pass")
+//     break;
+
+//     case "B":
+//     document.write("pass")
+//     break;
+
+//     case "C":
+//     document.write("pass")
+//     break;
+
+//     case "D":
+//     document.write("pass")
+//     break;
+
+//     case "F":
+//     document.write("fail")
+//     break;
+
+//     default:
+//         document.write("please enter your correct numbers")
+//         break;
+// }
+
+
+
+// let UserName = prompt("Please Enter Your UserName")
+
+// let user;
+// user= "Hasnain yaseen"
+
+
+// let password = prompt("Please Enter Your Password")
+
+// let pass;
+// pass="hasnain"
+
+// let pincode = prompt("Please Enter Your Pincode")
+
+// let pin;
+// pin="12345"
+
+
+
+// if ((UserName == "Hasnain yaseen" || email =="@gmail.com") && password === "hasnain"){
+//     console.log ("You have successfuly logged in")
+// }else{
+//     console.log("Invalid UserName & Password")
+// }
+
+// LOOPS
+
+// let a = 0;
+
+// while(a <=10){
+//     document.write("Hello world");
+//     a++;
+// }
+
+// let UserName = prompt("Please Enter Your UserName or Phonenumber")
+
+// let user;
+// user= "Hasnain yaseen"
+// phone="03112978674"
+
+// let password = prompt("Please Enter Your Password")
+
+// let pass;
+// pass="hasnain"
+
+// // let pincode = prompt("Please Enter Your Pincode")
+
+// // let pin;
+// // pin="12345"
+
+// if ((UserName === user || UserName === phone) && password === pass){
+//     console.log ("You have successfuly logged in")
+// }else{
+//     console.log("Invalid UserName & Password")
+// }
+
+// for (i =100; i >=1 ; i -=10){
+//     for(j=i; j > i - 10; j--){
+//         document.write(`${j} &nbsp`)
+//     }
+//     document.write(`<br>`)
+// }
+
+
+// for (i=1; i<=5; i++){
+//     for(j=1 ; j<=i; j++){
+//         document.write(`${i} &nbsp`)
+//     }
+//     document.write(`<br>`)
+// }
+
+
+// let product =[["https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/71Xcl-j5ipL._AC_UY218_.jpg","Audionic HeadPhone","SteelSeries Arctis 3 Console - Stereo Wired Gaming Headset for PlayStation 5 / 4, Xbox Series X|S, Nintendo Switch, VR, Android and iOS - White", 5600 ,1.5],
+
+// ["https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/714RMxN1sTL._AC_UY218_.jpg","Nova 7P","NEW SteelSeries Arctis Nova 7P Wireless Multi-Platform Gaming & Mobile Headset — Simultaneous Wireless 2.4GHz & Simultaneous Bluetooth — 38Hr Battery — USB-C — PS, PC", 4500 , 2.5],
+
+// ["https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/61CGHv6kmWL._AC_UY218_.jpg","Bengoo G9000","BENGOO G9000 Stereo Gaming Headset for PS4 PC Xbox One PS5 Controller, Noise Cancelling Over Ear Headphones with Mic, LED Light, Bass Surround, Soft Memory Earmuffs for Laptop", 6000 , 4.3]
+
+// ["https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/71BKQhFzDmL._AC_UY218_.jpg","Razer Karken"," Gaming Headset: Lightweight Aluminum Frame, Retractable Noise Isolating Microphone, For PC, PS4, PS5, Switch, Xbox One, Xbox Series X & S, Mobile, 3.5 mm Audio Jack", 5200 , 5.1]
+
+// ["https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/71waplSVO7L._AC_UY218_.jpg"]
+
+// ]
+
+
+// jpj
+// document.write(jpj)
+
+
+
+// let percentage= prompt("Enter your marks");
+
+// if (percentage >=70 && percentage <=100){
+//     if (percentage >= 90){
+//         let result = "A+"
+//         document.write("Your grade is "+ result);
+//     }else if (percentage >= 80){
+//         result = "A1"
+//         document.write("Your Grade is "+ result)
+//     }else if(percentage >= 70){
+//          result = "A"
+//         document.write("Your Grade is "+ result)
+//     }
+
+// }else if(percentage >= 60 && percentage <= 70){
+//      result = "B"
+//     document.write("Your Grade is "+ result)
+// }else if (percentage >= 50 && percentage <= 60){
+//   result = "C"
+//     document.write("Your Grade is " + result)
+// }else if (percentage >=40 && percentage <=50){
+//     result = "D"
+//     document.write("Your Grade is " + result)
+// }
+// else if (percentage < 40 && percentage <=40){
+//      result = "F"
+//     document.write("Your grade is " + result)
+// }else {
+//     document.write("Kindly enter valid percentage")
+// }
+// switch (result) {
+//     case "A+":
+//     document.write("pass")
+//     break;
+
+//     case "A1":
+//     document.write("pass")
+//     break;
+
+//     case "A":
+//     document.write("pass")
+//     break;
+
+//     case "B":
+//     document.write("pass")
+//     break;
+
+//     case "C":
+//     document.write("pass")
+//     break;
+
+//     case "D":
+//     document.write("pass")
+//     break;
+
+//     case "F":
+//     document.write("fail")
+//     break;
+
+//     default:
+//         document.write("please enter your correct numbers")
+//         break;
+// }
+
+
+// let UserName = prompt("Please Enter Your UserName or Phonenumber")
+
+// let user;
+// user= "Hasnain "
+// phone="03009255252"
+
+// let password = prompt("Please Enter Your Password")
+
+// let pass;
+// pass="1947"
+
+// if ((UserName === user || UserName === phone) && password === pass){
+//     console.log ("You have successfuly logged in")
+// }else{
+//     console.log("Invalid UserName & Password")
+// }
+
+
+
+
+
+// let UserName = prompt("Please Enter Your UserName")
+
+// let user;
+// user= "Hasnain"
+
+
+// let password = prompt("Please Enter Your Password")
+
+// let pass;
+// pass="1947"
+
+// // let pincode = prompt("Please Enter Your Pincode")
+
+// // let pin;
+// // pin="12345"
+
+// if ((UserName === user || UserName === phone) && password === pass){
+//     console.log ("You have successfuly logged in")
+// }else{
+//     console.log("Invalid UserName & Password")
+// }
+
+
+
+// let UserName = prompt("Please Enter Your UserName or Phonenumber")
+
+// let user;
+// user= "Hasnain Ali"
+// phone="03009255252"
+
+// let password = prompt("Please Enter Your Password")
+
+// let pass;
+// pass="hasnain"
+
+// let pincode = prompt("Please Enter Your Pincode")
+
+// let pin;
+// pin="12345"+
+
+// if ((UserName === user || UserName === phone) && password === pass){
+//     console.log ("You have successfuly logged in")
+// }else{
+//     console.log("Invalid UserName & Password")
+// }
+
+
+// let product =[["https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/71Xcl-j5ipL._AC_UY218_.jpg","Audionic HeadPhone","SteelSeries Arctis 3 Console - Stereo Wired Gaming Headset for PlayStation 5 / 4, Xbox Series X|S, Nintendo Switch, VR, Android and iOS - White", 5600 ,1.5],
+
+// ["https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/714RMxN1sTL._AC_UY218_.jpg","Nova 7P","NEW SteelSeries Arctis Nova 7P Wireless Multi-Platform Gaming & Mobile Headset — Simultaneous Wireless 2.4GHz & Simultaneous Bluetooth — 38Hr Battery — USB-C — PS, PC", 4500 , 2.5],
+
+// ["https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/61CGHv6kmWL._AC_UY218_.jpg","Bengoo G9000","BENGOO G9000 Stereo Gaming Headset for PS4 PC Xbox One PS5 Controller, Noise Cancelling Over Ear Headphones with Mic, LED Light, Bass Surround, Soft Memory Earmuffs for Laptop", 6000 , 4.3]
+
+// ["https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/71BKQhFzDmL._AC_UY218_.jpg","Razer Karken"," Gaming Headset: Lightweight Aluminum Frame, Retractable Noise Isolating Microphone, For PC, PS4, PS5, Switch, Xbox One, Xbox Series X & S, Mobile, 3.5 mm Audio Jack", 5200 , 5.1]
+
+// ["https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/71y2FyJ-s8L._AC_UY218_.jpg",""]
+
+// ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
